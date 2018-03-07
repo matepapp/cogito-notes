9d53e5b1c1d8ac625a16cb3351a0dd9a2c8fb321
Mate Papp <mate.papp@supercharge.io>
2018-03-07 13:32:56 +0100

    Test Travis with new button

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Button } from 'antd';
 import './App.css';
 
 class App extends Component {
+  handleButtonTap = () => {
+    alert('You tapped on button');
+  };
+
   render() {
     return (
       <div className="App">
-        <Button
-          type="primary"
-          onClick={() => {
-            alert('You tapped on button');
-          }}>
+        <Button type="primary" onClick={this.handleButtonTap}>
           Button
+        </Button>
+
+        <Button type="primary" onClick={this.handleButtonTap}>
+          Button2
         </Button>
       </div>
     );
