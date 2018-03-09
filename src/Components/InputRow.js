226d60fdb1c36703a8ab535216027f0364aa4b70
Kovacs-Egri Kristof <kovacsegrikristof@gmail.com>
2018-03-09 11:28:55 +0100

    Playing with layout alignment and grid layout.

--- src/Components/InputRow.js
+++ src/Components/InputRow.js
@@ -5,12 +5,9 @@ class InputRow extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <p>{this.props.label}:</p>
-        </div>
-        <div>
-          <Input placeholder={this.props.placeholder} />
-        </div>
+        <p>{this.props.label}:</p>
+
+        <Input placeholder={this.props.placeholder} />
       </div>
     );
   }
