b39f014fdd88b6c7ffe777f2243651f532874c49
Kovacs-Egri Kristof <kovacsegrikristof@gmail.com>
2018-03-15 19:03:52 +0100

    Login page updated to current design with separated style

--- src/Components/WrappedNormalLoginForm.js
+++ src/Components/WrappedNormalLoginForm.js
@@ -31,57 +31,55 @@ class NormalLoginForm extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <Card title="Regisztráció" style={{ textAlign: 'center' }}>
-        <Form
-          onSubmit={this.handleSubmit}
-          className="login-form"
-          style={{ width: '50%', margin: '0 auto' }}>
-          <FormItem>
-            {getFieldDecorator('lastName', {
-              rules: [{ required: true, message: 'Kérlek add meg a vezetékneved' }],
-            })(<Input placeholder="Vezetéknév" />)}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('firstName', {
-              rules: [{ required: true, message: 'Kérlek add meg az keresztneved!' }],
-            })(<Input placeholder="Keresztnév" />)}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('email', {
-              rules: [{ required: true, message: 'Kérlek add meg az e-mail címed!' }],
-            })(<Input placeholder="E-mail cím" />)}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Kérlek add meg a jelszavad!' }],
-            })(<Input type="password" placeholder="Jelszó" />)}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('passwordCheck', {
-              rules: [{ required: true, message: 'Kérlek add meg a jelszavad!' }],
-            })(<Input type="password" placeholder="Jelszó újra" />)}
-          </FormItem>
-          <FormItem style={{ textAlign: 'center' }}>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: false,
-            })(<Checkbox>ÁSZF elfogadása</Checkbox>)}
-            <Divider style={{ width: '100%' }} />
-            <Row>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-                style={{ width: '50%' }}>
-                Regisztráció
-              </Button>
-            </Row>
-            <Row>
-              Már van fiókod? <a href="">Jelentkezz be</a>
-            </Row>
-          </FormItem>
-        </Form>
-      </Card>
+      <Form
+        onSubmit={this.handleSubmit}
+        className="login-form"
+        style={{ width: '50%', margin: '0 auto' }}>
+        <FormItem>
+          {getFieldDecorator('lastName', {
+            rules: [{ required: true, message: 'Kérlek add meg a vezetékneved' }],
+          })(<Input placeholder="Vezetéknév" />)}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('firstName', {
+            rules: [{ required: true, message: 'Kérlek add meg az keresztneved!' }],
+          })(<Input placeholder="Keresztnév" />)}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('email', {
+            rules: [{ required: true, message: 'Kérlek add meg az e-mail címed!' }],
+          })(<Input placeholder="E-mail cím" />)}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Kérlek add meg a jelszavad!' }],
+          })(<Input type="password" placeholder="Jelszó" />)}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('passwordCheck', {
+            rules: [{ required: true, message: 'Kérlek add meg a jelszavad!' }],
+          })(<Input type="password" placeholder="Jelszó újra" />)}
+        </FormItem>
+        <FormItem style={{ textAlign: 'center' }}>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: false,
+          })(<Checkbox>ÁSZF elfogadása</Checkbox>)}
+          <Divider style={{ width: '100%' }} />
+          <Row>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              style={{ width: '50%' }}>
+              Regisztráció
+            </Button>
+          </Row>
+          <Row>
+            Már van fiókod? <a href="">Jelentkezz be</a>
+          </Row>
+        </FormItem>
+      </Form>
     );
   }
 }
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import WrappedNormalLoginForm from '../Components/WrappedNormalLoginForm';
 import { Layout, Row, Col } from 'antd';
-import logo from '../Resources/img/logo.png';
-const { Header, Footer, Content } = Layout;
+import logo from '../Resources/img/logo.svg';
+
+const { Header, Content } = Layout;
 
 const LoginPage = () => {
+  const styleLogo = {
+    marginTop: 50,
+    width: '25%',
+    height: '25%',
+  };
+
+  const styleCenter = {
+    display: 'flex',
+    justifyContent: 'center',
+  };
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Header style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Row>
-          <Col span={10} />
-          <Col span={4} style={{ display: 'flex', justifyContent: 'center' }}>
-            <img src={logo} alt="logo" />
-          </Col>
-        </Row>
-      </Header>
-      <Content style={{ paddingTop: 60 }}>
-        <Row type="flex" justify="center">
+      <Row style={styleCenter}>
+        <img src={logo} alt="logo" style={styleLogo} />
+      </Row>
+      <Content style={{ paddingTop: 70 }}>
+        <Row style={styleCenter}>
           <Col xs={20} sm={16} md={12} lg={8} xl={8}>
             <WrappedNormalLoginForm style={{}} />
           </Col>
