803f19e7a49b9eebacb7c7747a586bc6216295a9
Mate Papp <mate.papp@supercharge.io>
2018-03-18 17:01:45 +0100

    Rendering title in editor

--- src/Components/NoteEditor.js
+++ src/Components/NoteEditor.js
@@ -1,16 +1,39 @@
 // @flow
 import React from 'react';
-import { Row } from 'antd';
+import RichTextEditor from 'react-rte';
 
 type Props = {
-  isReadOnly: boolean,
   title: string,
+  onChange: Function,
 };
 
-const NoteEditor = (props: Props) => (
-  <Row>
-    <div>{props.title}</div>
-  </Row>
-);
+type State = {
+  value: any,
+};
+
+class NoteEditor extends React.Component<Props, State> {
+  state = {
+    value: RichTextEditor.createEmptyValue(),
+  };
+
+  onChange = (value: any) => {
+    this.setState({ value });
+    console.log(value.toString('markdown'));
+
+    // if (this.props.onChange) {
+    //   this.props.onChange(value.toString('html'));
+    // }
+  };
+
+  componentDidMount() {
+    this.setState({
+      value: RichTextEditor.createValueFromString(this.props.title, 'markdown'),
+    });
+  }
+
+  render() {
+    return <RichTextEditor value={this.state.value} onChange={this.onChange} />;
+  }
+}
 
 export default NoteEditor;
