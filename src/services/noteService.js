92be1c56b0d261f28ac5ca4d415df124fa0c1f26
Mate Papp <matepapp@icloud.com>
2018-04-15 14:11:30 +0200

    Create initial noteService

---
+++ src/services/noteService.js
@@ -0,0 +1,28 @@
+// @flow
+import { noteConstants } from '../constants';
+import { Note } from '../types';
+import { TOKEN } from '../helpers';
+import { network } from '.';
+
+const URL = {
+  LIST: '/notes/',
+};
+
+const config = {
+  headers: { Authorization: `JWT ${TOKEN}` },
+};
+const list = (): Promise<Note> => {
+  return network
+    .get(URL.LIST, config)
+    .catch(error => {
+      console.log(error);
+      return Promise.reject(error.response.data);
+    })
+    .then(response => {
+      console.log(response);
+    });
+};
+
+export const noteService = {
+  list,
+};
