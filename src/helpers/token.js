a674993a3484b5458dfcfaca0138b13732eae128
Mate Papp <matepapp@icloud.com>
2018-04-16 20:26:30 +0200

    Fix token set in header

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -3,12 +3,9 @@ import { authConstants } from '../constants';
 
 export const hasToken: boolean = localStorage.getItem(authConstants.TOKEN_KEY) != null;
 
-export const setToken = (token: string) => {
+export const setToken = (token: string) =>
   localStorage.setItem(authConstants.TOKEN_KEY, token);
-};
 
-export const removeToken = () => {
-  localStorage.removeItem(authConstants.TOKEN_KEY);
-};
+export const removeToken = () => localStorage.removeItem(authConstants.TOKEN_KEY);
 
 export const TOKEN = localStorage.getItem(authConstants.TOKEN_KEY);
